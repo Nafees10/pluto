4ab423aa773c31b531ed7e1938483ad5fd08fba6
Nafees Hassan <nafeeshassan16@gmail.com>
2023-06-25 23:02:45 +0500

    Added CLI interface

--- source/app.d
+++ source/app.d
@@ -1,6 +1,30 @@
-import std.stdio;
+import std.stdio,
+			 std.algorithm,
+			 std.path,
+			 std.file,
+			 std.string;
 
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
+import consts;
+
+int main(string[] args){
+	if (args.length < 2){
+		stderr.writeln(HELP_TEXT);
+		return 1;
+	}
+
+	if (["-h", "--help"].canFind(args[1])){
+		writeln(HELP_TEXT);
+		return 0;
+	}
+
+	if (["-v", "--version"].canFind(args[1])){
+		writeln(VERSION_INFO);
+		return 0;
+	}
+
+	string filename = args[1];
+	string outFilename = filename.chomp(".pluto") ~ ".plt";
+	if (args.length > 2)
+		outFilename = args[2];
+	return 0;
 }
